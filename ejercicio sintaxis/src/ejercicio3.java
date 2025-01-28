001bba2a295c2e69697ab6808878d3c9fa1fad34
Nao-CG <naomi.lncg@gmail.com>
2025-01-28 14:52:58 -0600

    finalizado

--- ejercicio sintaxis/src/ejercicio3.java
+++ ejercicio sintaxis/src/ejercicio3.java
@@ -5,12 +5,15 @@ public class ejercicio3 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("digita un numero entero: ");
         int x = scanner.nextInt();
+        int num = scanner.nextInt();
         int suma = 0;
 
         for (int i = 1; i <= x; i++) {
+        for (int i = 1; i <= num; i++) {
             suma += i;
         }
 
         System.out.println("La suma de todos los enteros desde 1 hasta " + x + " es: " + suma);
+        System.out.println("La suma de todos los enteros desde 1 hasta " + num + " es: " + suma);
     }
 }
