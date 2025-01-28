e98689ec05df9833ec6d5751e2065efb9a665dc7
Nao-CG <naomi.lncg@gmail.com>
2025-01-28 15:24:00 -0600

    finalizaado

---
+++ ejercicio sintaxis/src/ejercicio5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int PP=112;
+        int PM=75;
+
+        System.out.print("ingresa el numero de payasos: ");
+        int numP = scn.nextInt();
+
+        System.out.print("ingresa el numero de muñecas: ");
+        int numM = scn.nextInt();
+
+        int pesoTotal = (numP * PP) + (numM * PM);
+
+        System.out.println("el peso total es de: " + pesoTotal + " gramos");
+    }
+}
