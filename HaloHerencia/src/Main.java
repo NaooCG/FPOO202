f9f1ba320b76c04f7c7474c2160d4d722b296395
Nao-CG <naomi.lncg@gmail.com>
2025-03-06 14:50:23 -0600

    Practica No.14: Herencia & Polimorfismo

--- HaloHerencia/src/Main.java
+++ HaloHerencia/src/Main.java
@@ -2,6 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Spartanll jefemaestro=new Spartanll("jonh", 90, "rifle de asalto", 0);
+        jefemaestro.mostrarInfo();
+        jefemaestro.usarManejoAvanzado();
+        jefemaestro.atacar("covenant ");
+        jefemaestro.recibirDano(50);
+        jefemaestro.recargarEscudo();
 
+        Spartanlll nobleSix=new Spartanlll("Nao", 50, "espada de luz", 0);
+        nobleSix.mostrarInfo();
+        nobleSix.camuflajeActivo();
+        nobleSix.atacar("covenant ");
+        nobleSix.recibirDano(50);
+        nobleSix.recargarEscudo();
+
+        SpartanlV noble=new SpartanlV("Pablo", 70, "francotirador", 0);
+        noble.mostrarInfo();
+        noble.usarPropulsores();
+        noble.atacar("covenant ");
+        noble.recibirDano(50);
+        noble.recargarEscudo();
     }
 }
